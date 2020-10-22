c53ea694ec3ad388716a6401a5cf818951ab49bc
astro <astro@astro.localdomain.localdomain>
2020-10-22 09:55:21 -0600

    adding git pull

--- CLTool/flash.sh
+++ CLTool/flash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-make && ./cltool -c=/dev/ttyACM0 -uf=./IS_EVB-2_v1.8.2_b3_2020-07-30_165027.hex
+git pull && make && ./cltool -c=/dev/ttyACM0 -uf=./IS_EVB-2_v1.8.2_b3_2020-07-30_165027.hex
 
 if [ ! -d log ]
 then
