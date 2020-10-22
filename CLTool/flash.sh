b28ecc41c2559e6a0101513acc1201cb3d6fb6ff
astro <astro@astro.localdomain.localdomain>
2020-10-21 19:42:15 -0600

    moving log to sub folder

--- CLTool/flash.sh
+++ CLTool/flash.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 make && ./cltool -c=/dev/ttyACM0 -uf=./IS_EVB-2_v1.8.2_b3_2020-07-30_165027.hex
+
+mkdir log
+filetime=$(date '+%Y-%m-%d-%H%M%S')
+mv hexdump.log log/$filetime.log
